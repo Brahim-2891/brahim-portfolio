a37f6e0c55d7525174456e276e0316cbe647662a
Brahim AIT HADI <kuikfelbrahim@gmail.com>
2024-04-27 23:14:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    current_y = datetime.datetime.now().year
+    return render_template( 'index.html', c_year=current_y)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
